08b6e26c81af29a9dcf5371ad8e981de4f6cd1d8
Buğrahan <umutbugrahanozkan@gmail.com>
2021-04-05 15:33:48 +0300

    add product model

--- src/products/models.py
+++ src/products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    image = models.URLField(max_length=200)
+    url = models.URLField(max_length=200)
+
+    def __str__(self):
+        return self.name
